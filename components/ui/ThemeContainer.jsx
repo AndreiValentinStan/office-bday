239ece05c799a972f2c09021423de4a5ddd3b876
Bogdan Pirjol <bogdan.pirjol123@gmail.com>
2025-03-08 09:28:34 -0800

    Add theme manager
    Configured just to follow system theme

---
+++ components/ui/ThemeContainer.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function ThemeContainer(){
+    if(typeof window !== 'undefined'){
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches)
+            document.documentElement.classList.add('dark')
+    }
+        
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
+ export default {
+  content: [ 
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+   ],
+   darkMode: 'selector',
   theme: {
     extend: {
       colors: {
