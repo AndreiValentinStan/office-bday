70c6f2c4d7f412a8f3b17703bf9d3a8ce57979d6
Bogdan Pirjol <bogdan.pirjol123@gmail.com>
2025-03-08 12:02:45 -0800

    Add authentication pages
    Add login page and login form
    Add register page

---
+++ app/(authentication)/login/page.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "../../../components/authentication/LoginForm";
+
+
+export default function Login(){
+    return (<>
+        <div className="w-full h-dvh dark:bg-gray-900 bg-white ">
+            <LoginForm />
+        </div>
+    </>)
+}
