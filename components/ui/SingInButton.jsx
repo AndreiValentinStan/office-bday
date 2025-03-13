b041f18c920436f5b5397bc6c7ae40e6ff6a0a6c
Bogdan Pirjol <bogdan.pirjol123@gmail.com>
2025-03-13 06:12:47 -0700

    Add sign in logic in SignInButton component

--- components/ui/SingInButton.jsx
+++ components/ui/SingInButton.jsx
@@ -1,12 +1,35 @@
-'use client'
+"use client";
 
-export default function SignInButton({ text, style }) {
-  const submitHandler = (e) => {
+export default function SignInButton({ text, style, setError }) {
+  const submitHandler = async (e) => {
     e.preventDefault();
     let [email, password] = e.target.form || [];
     email = email.value;
     password = password.value;
-    console.log({ email, password });
+    if (!email || !password) {
+      setError("Please provide email and password");
+      return;
+    }
+    try {
+      const response = await fetch("/api/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      if (!response || !response.ok) {
+        setError("Couldn`t process your request!");
+        return;
+      }
+      const data = await response.json();
+      if (data?.message) console.log(data.message);
+    } catch (err) {
+      console.log("Error on signing in ");
+    }
   };
   return (
     <>
